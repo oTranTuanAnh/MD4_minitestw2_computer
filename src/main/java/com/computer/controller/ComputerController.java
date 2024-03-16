2e17b7c52d103940e46a933dc1900e1d93de929b
viethungle1 <hunglv0801@gmail.com>
2024-03-16 21:04:35 +0700

    add crud RESTful computer

--- src/main/java/com/computer/controller/ComputerController.java
+++ src/main/java/com/computer/controller/ComputerController.java
@@ -1,7 +1,57 @@
 package com.computer.controller;
 
+import com.computer.model.entity.Computer;
+import com.computer.service.computerservice.ComputerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/api/computer")
 public class ComputerController {
+
+    @Autowired
+    private ComputerService computerService;
+
+    @GetMapping()
+    public ResponseEntity<Iterable<Computer>> findAllComputer() {
+        Iterable<Computer> computers = computerService.findAll();
+        return new ResponseEntity<>(computers, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Computer> findComputerById (@PathVariable Long id) {
+        Optional<Computer> customer = computerService.findById(id);
+        if (!customer.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(customer.get(),HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Computer> saveComputer(@RequestBody Computer customer) {
+        return new ResponseEntity<>(computerService.save(customer),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Computer> updateComputer(@PathVariable Long id, @RequestBody Computer computer) {
+        Optional<Computer> optionalComputer = computerService.findById(id);
+        if (!optionalComputer.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        computer.setId(optionalComputer.get().getId());
+        return new ResponseEntity<>(computerService.save(computer),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Computer> deleteCustomer(@PathVariable Long id) {
+        Optional<Computer> computerOptional = computerService.findById(id);
+        if (!computerOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        computerService.remove(id);
+        return new ResponseEntity<>(computerOptional.get(),HttpStatus.OK);
+    }
 }
--- src/main/java/com/computer/service/computerservice/ComputerService.java
+++ src/main/java/com/computer/service/computerservice/ComputerService.java
@@ -1,28 +1,33 @@
 package com.computer.service.computerservice;
 
 import com.computer.model.entity.Computer;
+import com.computer.repo.computer.IComputerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 @Service
 public class ComputerService implements IComputerService{
+    @Autowired
+    private IComputerRepo computerRepo;
+
     @Override
     public Iterable<Computer> findAll() {
-        return null;
+        return computerRepo.findAll();
     }
 
     @Override
     public Optional<Computer> findById(Long id) {
-        return Optional.empty();
+        return computerRepo.findById(id);
     }
 
     @Override
     public Computer save(Computer computer) {
-        return null;
+        return computerRepo.save(computer);
     }
 
     @Override
     public void remove(Long id) {
-
+        computerRepo.deleteById(id);
     }
 }
